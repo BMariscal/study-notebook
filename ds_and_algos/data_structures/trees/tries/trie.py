19ba3b7d08039eb0c503b3811089126f4bf2f1a9
Briceida <BriceidaMariscal@gmail.com>
2019-12-01 20:10:09 -0800

    tries

---
+++ ds_and_algos/data_structures/trees/tries/trie.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+
+
+class TrieNode(object):
+    def __init__(self, char: str):
+        self.char = char
+        self.children = []
+        self.word_finished = False
+        self.counter = 1
+
+
+def add(root, word: str):
+    node = root
+    for char in word:
+        found_in_child = False
+        for child in node.children:
+            if child.char == char:
+                child.counter += 1
+                node = child
+                found_in_child = True
+                break
+        if not found_in_child:
+            new_node = TrieNode(char)
+            node.children.append(new_node)
+            node = new_node
+    node.word_finished = True
+
+
+def find_prefix(root, prefix: str) -> Tuple[bool, int]:
+    node = root
+    if not root.children:
+        return False, 0
+    for char in prefix:
+        char_not_found = True
+        for child in node.children:
+            if child.char == char:
+                char_not_found = False
+                node = child
+                break
+        if char_not_found:
+            return False, 0
+    return True, node.counter
+
+
+if __name__ == "__main__":
+    root = TrieNode('*')
+    add(root, "cookiemonster")
+    add(root, 'cookie')
+
+    print(find_prefix(root, 'coo'))
+    print(find_prefix(root, 'cook'))
+    print(find_prefix(root, 'cookiemonster'))
+    print(find_prefix(root, 'cookie'))
+    print(find_prefix(root, 'cookiemon'))
+    print(find_prefix(root, 'cookiechow'))
+    print(find_prefix(root, 'cow'))
+    print(find_prefix(root, 'chow'))
